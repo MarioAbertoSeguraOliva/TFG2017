eba27ce569556c00c566f6185c852609fd1b469e
Mario Alberto Segura Oliva <marioalbertoseguraoliva@gmail.com>
2017-09-26 01:41:01 +0100

    Added Coach, User & Athlete Classes to -war package users

---
+++ TFG2017/TFG2017-war/src/java/users/Athlete.java
@@ -0,0 +1,12 @@
+package users;
+
+public class Athlete extends User{
+    private String name;
+    private String surname;
+    private String dateOfBirth;
+    private String sex;    
+    private String goal;    
+    private String accountType;    
+    private String accountStatus;
+    private String creationDate;
+}
---
+++ TFG2017/TFG2017-war/test/users/Coach_.java
@@ -0,0 +1,36 @@
+package users;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class Coach_ {
+    
+    public Coach_() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
